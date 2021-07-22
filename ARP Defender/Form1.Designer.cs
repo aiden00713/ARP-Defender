488444068780400729df8be89276337205bf0fc7
aiden00713 <44128720+aiden00713@users.noreply.github.com>
2021-07-23 01:34:48 +0800

    ver.01

---
+++ ARP Defender/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace ARP_Defender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.gatewaymac_label = new System.Windows.Forms.Label();
+            this.gatewayip_label = new System.Windows.Forms.Label();
+            this.hostmac_label = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hostip_label = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.show_label = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.start.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.start.ForeColor = System.Drawing.Color.White;
+            this.start.Location = new System.Drawing.Point(150, 50);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(165, 70);
+            this.start.TabIndex = 0;
+            this.start.Text = "開始";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.stop.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.stop.ForeColor = System.Drawing.Color.White;
+            this.stop.Location = new System.Drawing.Point(450, 50);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(165, 70);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "停止";
+            this.stop.UseVisualStyleBackColor = false;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.73485F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70.26515F));
+            this.tableLayoutPanel1.Controls.Add(this.gatewaymac_label, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.gatewayip_label, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.hostmac_label, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.hostip_label, 1, 0);
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(89, 250);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(586, 208);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // gatewaymac_label
+            // 
+            this.gatewaymac_label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gatewaymac_label.AutoSize = true;
+            this.gatewaymac_label.Location = new System.Drawing.Point(178, 154);
+            this.gatewaymac_label.Name = "gatewaymac_label";
+            this.gatewaymac_label.Size = new System.Drawing.Size(404, 53);
+            this.gatewaymac_label.TabIndex = 7;
+            this.gatewaymac_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gatewayip_label
+            // 
+            this.gatewayip_label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gatewayip_label.AutoSize = true;
+            this.gatewayip_label.Location = new System.Drawing.Point(178, 103);
+            this.gatewayip_label.Name = "gatewayip_label";
+            this.gatewayip_label.Size = new System.Drawing.Size(404, 50);
+            this.gatewayip_label.TabIndex = 6;
+            this.gatewayip_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // hostmac_label
+            // 
+            this.hostmac_label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.hostmac_label.AutoSize = true;
+            this.hostmac_label.Location = new System.Drawing.Point(178, 52);
+            this.hostmac_label.Name = "hostmac_label";
+            this.hostmac_label.Size = new System.Drawing.Size(404, 50);
+            this.hostmac_label.TabIndex = 5;
+            this.hostmac_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 53);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "預設閘道 MAC 位址";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "主機 IP 位址";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 50);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "主機 MAC 位址";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 50);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "預設閘道 IP 位址";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // hostip_label
+            // 
+            this.hostip_label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.hostip_label.AutoSize = true;
+            this.hostip_label.Location = new System.Drawing.Point(178, 1);
+            this.hostip_label.Name = "hostip_label";
+            this.hostip_label.Size = new System.Drawing.Size(404, 50);
+            this.hostip_label.TabIndex = 4;
+            this.hostip_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(6, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(210, 41);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "目前防禦狀態";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // show_label
+            // 
+            this.show_label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.show_label.AutoSize = true;
+            this.show_label.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.show_label.ForeColor = System.Drawing.Color.Red;
+            this.show_label.Location = new System.Drawing.Point(6, 47);
+            this.show_label.Name = "show_label";
+            this.show_label.Size = new System.Drawing.Size(210, 42);
+            this.show_label.TabIndex = 4;
+            this.show_label.Text = "尚未開啟防禦";
+            this.show_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.OutsetPartial;
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.label5, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.show_label, 0, 1);
+            this.tableLayoutPanel2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(271, 138);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(222, 92);
+            this.tableLayoutPanel2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "ARP Defender";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label hostmac_label;
+        private System.Windows.Forms.Label hostip_label;
+        private System.Windows.Forms.Label gatewayip_label;
+        private System.Windows.Forms.Label gatewaymac_label;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label show_label;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+    }
+}
+
---
+++ ARP Defender/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace ARP_Defender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); //初始化組件
+            hostip_label.Text = GetHostIPAddress();
+            hostmac_label.Text = GetHostMACAddress();
+            gatewayip_label.Text = GetGatewayIPAddress().ToString();
+            gatewaymac_label.Text = GetGatewayMACAddress(GetGatewayIPAddress().ToString());
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            show_label.Text = "開啟防禦";
+            show_label.ForeColor = Color.Green;
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            show_label.Text = "尚未開啟防禦";
+            show_label.ForeColor = Color.Red;
+        }
+
+        public string GetHostIPAddress()
+        {
+            string localIP = string.Empty;
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
+            socket.Connect("8.8.8.8", 65530);
+            IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+            localIP = endPoint.Address.ToString();
+
+            return localIP;
+        }
+
+        public string GetHostMACAddress()
+        {
+            Dictionary<string, long> macAddresses = new Dictionary<string, long>();
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                
+                if (nic.OperationalStatus == OperationalStatus.Up)
+                {
+                    macAddresses[nic.GetPhysicalAddress().ToString()] = nic.GetIPStatistics().BytesSent + nic.GetIPStatistics().BytesReceived;
+                }    
+            }
+            long maxValue = 0;
+            string mac = "";
+            foreach (KeyValuePair<string, long> pair in macAddresses)
+            {
+                if (pair.Value > maxValue)
+                {
+                    mac = pair.Key;
+                    maxValue = pair.Value;
+                }
+            }
+            var regex = "(.{2})(.{2})(.{2})(.{2})(.{2})(.{2})";
+            var replace = "$1-$2-$3-$4-$5-$6";
+            var mac_newformat = Regex.Replace(mac, regex, replace);
+            return mac_newformat.ToString();
+        }
+
+        public static IPAddress GetGatewayIPAddress(IPAddress netaddr = null)
+        {
+            // user can provide an ip address that exists on the network they want to connect to, 
+            // or this routine will default to 1.1.1.1 (IP of a popular internet dns provider)
+            if (netaddr is null)
+            {
+                netaddr = IPAddress.Parse("8.8.8.8");
+            }
+                
+            PingReply reply = default;
+            var ping = new Ping();
+            var options = new PingOptions(1, true); // ttl=1, dont fragment=true
+            try
+            {
+                // I arbitrarily used a 200ms timeout; tune as you see fit.
+                reply = ping.Send(netaddr, 200, new byte[0], options);
+            }
+            catch (PingException)
+            {
+                System.Diagnostics.Debug.WriteLine("Gateway not available");
+                return default;
+            }
+            if (reply.Status != IPStatus.TtlExpired)
+            {
+                System.Diagnostics.Debug.WriteLine("Gateway not available");
+                return default;
+            }
+            return reply.Address;
+        }
+
+        public string GetGatewayMACAddress(string GatewayIP)
+        {
+            string dirResults = "";
+            ProcessStartInfo psi = new ProcessStartInfo();
+            Process proc = new Process();
+            psi.FileName = "arp";
+            psi.RedirectStandardInput = false;
+            psi.RedirectStandardOutput = true;
+            psi.Arguments = "-a " + GatewayIP;
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            try
+            {
+                proc = Process.Start(psi);
+                dirResults = proc.StandardOutput.ReadToEnd();
+                proc.WaitForExit();
+            }
+            catch (Exception)
+            { }
+
+            Match m = Regex.Match(dirResults, "\\w+\\-\\w+\\-\\w+\\-\\w+\\-\\w+\\-\\w\\w");
+
+            if (m.ToString() != "")
+            {
+                return m.ToString();
+            }
+            else
+            {
+                return "找不到GatewayMACAddress";
+            }
+        }
+
+
+    }
+}
